240291188670fc79cf1c7e75c3eeb2d03a566ba8
Nguyễn Thành Hưng <nguyenthanhhung19872004@gmail.com>
2019-09-23 11:07:02 +0700

    Initial Commit

---
+++ NewsapiImplement/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  NewsapiImplement
+//
+//  Created by Hung Nguyen Thanh on 9/23/19.
+//  Copyright © 2019 Hung Nguyen Thanh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ NewsapiImplement/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  NewsapiImplement
+//
+//  Created by Hung Nguyen Thanh on 9/23/19.
+//  Copyright © 2019 Hung Nguyen Thanh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
